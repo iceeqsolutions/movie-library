4e99d652de6cfd74a60a362fda52b07650449a44
Fleege <76089935+pfleege@users.noreply.github.com>
2024-11-05 13:18:55 +0200

    ADD Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Navbar from "./components/Navbar";
 import "./styles/App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
