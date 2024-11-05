a9dc68901666da763f017fb8ee24b31d34201188
Fleege <76089935+pfleege@users.noreply.github.com>
2024-11-05 13:28:03 +0200

    ADD css module + install necessary packages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,15 @@
+import { NavbarContainer } from "../styles/Styles.modules";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <NavbarContainer>
+      <h1>Movie Library</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
+        assumenda optio vitae voluptate ratione maiores velit voluptatibus ex
+        explicabo eveniet.
+      </p>
+    </NavbarContainer>
+  );
 };
 
 export default Navbar;
