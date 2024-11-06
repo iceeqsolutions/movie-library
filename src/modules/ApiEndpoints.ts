bf58ae64a755a09de575befe7e6c4f572b8b5fa6
Fleege <76089935+pfleege@users.noreply.github.com>
2024-11-06 15:30:02 +0200

    MOD environment variable reference in ApiEndpoints
    - Vite uses import.meta.env instead of process.env.
    - Vite requires its environment variables to be prefixed with VITE_.

--- src/modules/ApiEndpoints.ts
+++ src/modules/ApiEndpoints.ts
@@ -1,23 +1,23 @@
-const apiKey = process.env.API_KEY;
+const apiKey: string = import.meta.env.VITE_API_KEY;
 
 // Movies
 export const trending_movies =
   "https://api.themoviedb.org/3/trending/movie/week?api_key=" + apiKey;
 export const upcoming_movies =
-  "https://api.themoviedb.org/3/movie/upcoming" + apiKey;
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=" + apiKey;
 export const popular_movies =
-  "https://api.themoviedb.org/3/movie/popular" + apiKey;
+  "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey;
 export const top_rated_movies =
-  "https://api.themoviedb.org/3/movie/top_rated" + apiKey;
+  "https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiKey;
 export const movies_now_playing =
-  "https://api.themoviedb.org/3/movie/now_playing" + apiKey;
+  "https://api.themoviedb.org/3/movie/now_playing?api_key=" + apiKey;
 
 // TV Shows
 export const trending_tv_shows =
-  "https://api.themoviedb.org/3/trending/tv/week" + apiKey;
+  "https://api.themoviedb.org/3/trending/tv/week?api_key=" + apiKey;
 export const popular_tv_shows =
-  "https://api.themoviedb.org/3/tv/popular" + apiKey;
+  "https://api.themoviedb.org/3/tv/popular?api_key=" + apiKey;
 export const top_rated_tv_shows =
-  "https://api.themoviedb.org/3/tv/top_rated" + apiKey;
+  "https://api.themoviedb.org/3/tv/top_rated?api_key=" + apiKey;
 export const tv_shows_airing_today =
-  "https://api.themoviedb.org/3/tv/airing_today" + apiKey;
+  "https://api.themoviedb.org/3/tv/airing_today?api_key=" + apiKey;
