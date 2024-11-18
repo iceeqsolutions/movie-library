5b81c2d6c9edfbcabb69746081deab17a47ea32d
Fleege <76089935+pfleege@users.noreply.github.com>
2024-11-18 17:16:19 +0200

    ADD styling to movie details page

--- src/components/ApiMovieDetails.tsx
+++ src/components/ApiMovieDetails.tsx
@@ -1,20 +1,22 @@
-import { ApiDataItemsContainer } from "../styles/Styles.modules";
+import { MovieDetailsContainer } from "../styles/Styles.modules";
 import {
   ApiMovieItemProps,
   ApiDataProps,
 } from "../interfaces/ApiMovieDetailsInterfaces";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import CircularProgress from "@mui/material/CircularProgress";
 
 /*
-Poster path - https://image.tmdb.org/t/p/w500${item.poster_path}
-Backdrop path - https://image.tmdb.org/t/p/w500${item.backdrop_path} 
-IMDB link - https://www.imdb.com/title/${item.imdb_id}/
+Poster path - https://image.tmdb.org/t/p/w500${showData.poster_path}
+Backdrop path - https://image.tmdb.org/t/p/w500${showData.backdrop_path} 
+IMDB link - https://www.imdb.com/title/${showData.imdb_id}/
 
 */
 
 const ApiMovieDetails: React.FC<ApiDataProps> = ({ apiEndpoint }) => {
   const [showData, setShowData] = useState<ApiMovieItemProps | null>(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!apiEndpoint) {
@@ -35,24 +37,102 @@ const ApiMovieDetails: React.FC<ApiDataProps> = ({ apiEndpoint }) => {
       console.error("API endpoint is undefined");
       return;
     }
+    setTimeout(() => {
+      setLoading(true);
+    }, 500);
   }, [apiEndpoint]);
 
   if (!showData) {
     return (
-      <ApiDataItemsContainer>
+      <MovieDetailsContainer>
         <p>No data available...</p>
-      </ApiDataItemsContainer>
+      </MovieDetailsContainer>
     );
   }
 
+  /*   if (loading) {
+    return (
+      <ApiDataItemsContainer>
+        <div className="loadingCircle">
+          <CircularProgress size={70} color="warning" />
+          <p>Loading...</p>
+        </div>
+      </ApiDataItemsContainer>
+    );
+  } */
+  const releaseYear =
+    showData.release_date || showData.first_air_date
+      ? showData.release_date
+        ? showData.release_date.substring(0, 4)
+        : showData.first_air_date.substring(0, 4)
+      : "N/A";
+
   return (
-    <ApiDataItemsContainer>
+    <MovieDetailsContainer>
+      {!loading && (
+        <div className="loadingCircle">
+          <CircularProgress size={70} color="warning" />
+          <p>Loading...</p>
+        </div>
+      )}
       <div className="mainContainer">
         <div className="section">
-          <h2>{showData.title}</h2>
+          <h1>{showData.title}</h1>
+          <div key={showData.id} className="card">
+            <div className="cardTop">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${showData.poster_path}`}
+                alt={showData.title}
+              />
+              <div className="detailsContainer">
+                {showData.backdrop_path ? (
+                  <img
+                    className="backdrop"
+                    src={`https://image.tmdb.org/t/p/w500${showData.backdrop_path}`}
+                    alt={showData.title}
+                  />
+                ) : null}
+                <p className="overview">{showData.overview}</p>
+              </div>
+            </div>
+            <div className="cardMiddle">
+              <h3>Release year: {releaseYear}</h3>
+              <h3>
+                Rating:{" "}
+                <span className="rating">
+                  {showData.vote_average.toFixed(1)}
+                </span>
+              </h3>
+            </div>
+            <div className="cardBottom">
+              <div className="left">
+                <p>Movie length: {showData.runtime} minutes</p>
+                <p>Movie language: {showData.original_language}</p>
+                <p>
+                  Homepage:{" "}
+                  <a href={showData.homepage} target="_blank">
+                    {showData.title} Official Homepage
+                  </a>
+                </p>
+                <p>
+                  IMDB:{" "}
+                  <a
+                    href={`https://www.imdb.com/title/${showData.imdb_id}/`}
+                    target="_blank"
+                  >
+                    {showData.title}
+                  </a>
+                </p>
+              </div>
+              <div className="right">
+                <p>Movie budget: ${showData.budget}</p>
+                <p>Generated revenue: ${showData.revenue}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </ApiDataItemsContainer>
+    </MovieDetailsContainer>
   );
 };
 
--- src/styles/Styles.modules.ts
+++ src/styles/Styles.modules.ts
@@ -342,3 +342,178 @@ export const SearchBarContainer = styled.div`
     }
   }
 `;
+
+export const MovieDetailsContainer = styled.div`
+  @font-face {
+    font-family: "ApiDataRecord";
+    src: url("src/assets/GomariceCurryBread.ttf") format("truetype");
+    font-weight: 400;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: "MovieDescription";
+    src: url("src/assets/AshemarkRegular.otf") format("opentype");
+    font-weight: 400;
+    font-style: normal;
+  }
+  display: flex;
+  justify-content: center;
+  margin-top: 270px;
+  background-color: #111;
+  border-radius: 10px;
+  .loadingCircle {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    color: #fff;
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(3, 40, 65, 0.4);
+    > p {
+      font-family: "MovieDescription";
+      font-size: 2rem;
+      letter-spacing: 2px;
+      padding-top: 10px;
+    }
+  }
+  .mainContainer {
+    display: flex;
+    width: 56%;
+    flex-direction: column;
+    align-items: center;
+
+    .section {
+      font-family: "LinksFont";
+      padding-bottom: 50px;
+      > h1 {
+        color: #a87d32;
+        text-shadow: 0 0 1px #fff;
+        letter-spacing: 2px;
+        font-size: 4rem;
+        font-weight: 200;
+        padding: 20px 50px;
+      }
+      .card {
+        display: flex;
+        flex-direction: column;
+        margin: 20px;
+        border-radius: 15px;
+        box-shadow: 0 0 10px 10px #fff;
+        .cardTop {
+          display: flex;
+          > img {
+            width: 400px;
+            height: 560px;
+            margin: 20px 10px 20px 20px;
+            border: 2px solid #a87d32;
+            border-radius: 10px;
+            /* box-shadow: 0 0 10px 10px #fff; */
+          }
+          > .detailsContainer {
+            display: flex;
+            flex-direction: column;
+            /* margin-left: 20px; */
+            > p {
+              font-family: "MovieDescription";
+              color: #ddd;
+              padding: 10px;
+              font-size: 1.5rem;
+            }
+            .overview {
+              background-color: #222;
+              color: #ddd;
+              margin: 10px 20px 20px 10px;
+              padding: 10px;
+              border: 2px solid #a87d32;
+              border-radius: 10px;
+              font-size: 1.5rem;
+              width: 400px;
+              height: 290px;
+              overflow: auto;
+              scrollbar-width: none;
+              scrollbar-color: #a87d32 #bbb;
+            }
+            .backdrop {
+              width: 420px;
+              margin: 20px 20px 10px 10px;
+              border: 2px solid #a87d32;
+              border-radius: 10px;
+              height: 225px;
+            }
+          }
+        }
+        .cardMiddle {
+          display: flex;
+          justify-content: space-around;
+          h3 {
+            font-family: "MovieDescription";
+            color: #ddd;
+            padding: 10px;
+            font-size: 2.2rem;
+            .rating {
+              border: 4px solid #6b5716;
+              border-radius: 50%;
+              background-color: #a87d32;
+              padding: 10px 12px;
+              color: #fff;
+              font-weight: 600;
+            }
+          }
+        }
+        .cardBottom {
+          display: flex;
+          justify-content: space-around;
+          padding-bottom: 60px;
+          .left {
+            display: flex;
+            flex-direction: column;
+            /* padding: 10px;
+            h3 {
+              font-family: "MovieDescription";
+              color: #ddd;
+              padding: 10px;
+              font-size: 2.2rem;
+            } */
+            p {
+              font-family: "MovieDescription";
+              color: #ddd;
+              margin: 0;
+              /* padding: 10px; */
+              font-size: 1.5rem;
+              a {
+                color: #a87d32;
+                text-decoration: none;
+                &:hover {
+                  text-decoration: underline;
+                }
+              }
+            }
+          }
+          .right {
+            display: flex;
+            flex-direction: column;
+            /* padding: 10px;
+            h3 {
+              font-family: "MovieDescription";
+              color: #ddd;
+              padding: 10px;
+              font-size: 2.2rem;
+            } */
+            p {
+              font-family: "MovieDescription";
+              color: #ddd;
+              margin: 0;
+              /* padding: 10px; */
+              font-size: 1.5rem;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
